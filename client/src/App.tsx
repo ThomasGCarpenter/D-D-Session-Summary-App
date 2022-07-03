5ec591bda9509f4f6ee2ccc0de7fbf74fe53ba02
Thomas Carpenter <tcarp92@gmail.com>
2022-07-03 16:35:29 -0500

    Added description of app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DnD Session App
+          DnD Session App!!! Awesome Fun!
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
