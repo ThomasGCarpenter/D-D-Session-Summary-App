7a555e6ef95656db13256f8f0f257677e44bb598
Miles Dickinson <miles.h.dickinson@gmail.com>
2022-07-21 21:33:41 -0400

    Improved welcome page landing UI

--- client/src/pages/welcome.tsx
+++ client/src/pages/welcome.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import './welcome.css';
 
 function Welcome() {
     return(
-        <p>hello</p>
+        <div className="welcome">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6">
+                        <div className="welcome__module">
+                            Welcome Message
+                        </div>
+                    </div>
+                    <div className="col-xs-12 col-sm-6">
+                        <div className="welcome__module">
+                            <button className="btn btn-primary">
+                                Sign In
+                            </button>
+                            <button className="btn btn-primary">
+                                Sign Up
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
