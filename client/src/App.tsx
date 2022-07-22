f2ffcb303174d8440d281f1da7fed6838aca4f7c
Thomas Carpenter <tcarp92@gmail.com>
2022-07-21 21:17:35 -0400

    Added Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './pages/welcome'
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DnD Session App!!! Awesome Fun!
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Welcome />
     </div>
   );
 }
---
+++ client/src/components/header.tsx
@@ -0,0 +1,22 @@
+function Header(){
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Dnd App</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Sign In <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Sing Up</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    )
+}
+
+export default Header;
